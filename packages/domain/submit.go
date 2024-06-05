040c702da32c7d6c9c78bfc1f6c19a6db8892421
ISongwut <isongwut.me@gmail.com>
2024-06-05 21:38:19 +0700

    mod: fix some code.

--- packages/domain/submit.go
+++ packages/domain/submit.go
@@ -1,8 +1,11 @@
 package domain
 
-type Submit struct {
-	Language string `json:"lang"`
-	Code string `json:"code"`
-	SubmitAt string `json:"submitAt"`
-	Status string `json:"status"`
-}
+type (
+	Submit struct {
+		Language string `json:"lang"`
+		Code     string `json:"code"`
+		SubmitAt string `json:"submitAt"`
+		Status   string `json:"status"`
+	}
+)
+
